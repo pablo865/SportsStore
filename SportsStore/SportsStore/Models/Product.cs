6fa8d8fdd2d9ab2a081d791e71a47656900d97ed
Paweł Piekutowski <futureiscode@gmail.com>
2018-11-22 15:45:20 +0100

    Tworzenie repozytorium

--- SportsStore/SportsStore/Models/Product.cs
+++ SportsStore/SportsStore/Models/Product.cs
@@ -9,6 +9,9 @@ namespace SportsStore.Models
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
 
     }
 }
